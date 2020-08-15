526b79978b7bf4754acd5c1dcceff8719b3f9f69
Daniel <borzecki.daniel@gmail.com>
2020-08-15 18:41:53 +0200

    Updates

---
+++ precache-manifest.8ae41cacae9f2443e58410091273a654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac900dc51e04f430f645ec34b01d7aad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b5f223983fc1ca534c6",
+    "url": "/static/css/main.e93931ce.chunk.css"
+  },
+  {
+    "revision": "cddcde019e897837c938",
+    "url": "/static/js/2.763520bf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.763520bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b5f223983fc1ca534c6",
+    "url": "/static/js/main.7f16786d.chunk.js"
+  },
+  {
+    "revision": "d873ce1c64470a450c88",
+    "url": "/static/js/runtime-main.01ff1907.js"
+  }
+]);
